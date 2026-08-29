d185e9c29bf1fcd057535a9ff10447c5c3acb155
aman sahu <amansahu22052004@gmail.com>
2026-08-29 23:26:34 +0530

    add urls

--- apps/Profile/api/urls.py
+++ apps/Profile/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from.views import ProfileView
+from.views import ProfileView,ProflieLikeView,GetOtherUserProfile
 
 urlpatterns = [
-    path("me/",ProfileView.as_view(),)
+    path("me/",ProfileView.as_view(),name='user_profile'),
+    path('profleLike/<uuid:Profile_id>/like/',ProflieLikeView.as_view(),name='profile-like'),
+    path('other_profile/<uuid:user_id>/',GetOtherUserProfile.as_view(),name="get_profile")
 ]
