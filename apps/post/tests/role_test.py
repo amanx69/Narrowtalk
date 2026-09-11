67e0c6cd8f68cc45be67339640ce11f01416cefa
aman sahu <amansahu22052004@gmail.com>
2026-09-11 14:03:46 +0530

    write left tests for role

--- apps/post/tests/role_test.py
+++ apps/post/tests/role_test.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.core.cache import cache
 User= get_user_model()
 
 
@@ -13,6 +14,7 @@ User= get_user_model()
 class TestRoleClass(APITestCase):
     
     def setUp(self):
+        cache.clear()
         
         self.user=User.objects.create_user(
             email="aman@gmail.com",
@@ -25,24 +27,34 @@ class TestRoleClass(APITestCase):
             description="This is a test project.",
             
         )
+        
         self.role= RoleNeeded.objects.create(
                 title="i wnat to best dev",
                 description="hyy brothr can you make to this project",
-                project=self.project
+                project=self.project,
+            
+            
             )
+        
+        
         self.url=reverse('v1:post_urls-roles', args=[self.project.id])
         token=RefreshToken.for_user(self.user)
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {str(token.access_token)}")
-        
-   
+      
+    
     def test_created_role(self):
         url=reverse('v1:post_urls-roles', args=[self.project.id])
         data={
                 "title":"Test Role",
                 "description":"This is a test role.",
                 "slots_available":2,
+                "required_skills": [
+                 {"name": "Django"},
+                 {"name": "Python"},
+        ],
+              
             }
-        res=self.client.post(url,data)
+        res=self.client.post(url,data, format='json')
         self.assertEqual(res.status_code,status.HTTP_201_CREATED)
         
     def test_create_role_unauthorized_user(self):
@@ -59,11 +71,24 @@ class TestRoleClass(APITestCase):
     def test_get_roles(self):
         url=reverse('v1:post_urls-roles', args=[self.project.id])
         res=self.client.get(url)
+
         self.assertEqual(res.status_code,status.HTTP_200_OK)
         
     def test_missing_title(self):
         res=self.client.post(self.url,{"title":"","description":"vmdnbchdcdcdhcbjdc"})
         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+        
+    def test_without_skill(self):
+        
+        data={
+            "title":"Test Role",
+            "description":"This is a test role.",
+            "slots_available":2
+                          
+            }
+        res=self.client.post(self.url,data,format="json")
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+        
     def test_missing_dec(self):
         res=self.client.post(self.url,{"title":"anshu kuamri my life line my love ❤️","description":""})
         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
@@ -95,6 +120,49 @@ class TestRoleClass(APITestCase):
         url=reverse('v1:post_urls-edit_and_get_role',args=[self.project.id,self.role.id])
         res=self.client.get(url,format='json')
         self.assertEqual(res.status_code,status.HTTP_200_OK)
+        
+    def test_role_detiels_anou_user(self):
+        self.client.credentials()
+        url=reverse('v1:post_urls-edit_and_get_role',args=[self.project.id,self.role.id])
+        res=self.client.get(url,format='json')
+        self.assertEqual(res.status_code,status.HTTP_401_UNAUTHORIZED)
+        
+    def test_update_role(self):
+        url=reverse('v1:post_urls-edit_and_get_role',args=[self.project.id,self.role.id])
+        data={
+            "title":"aman kumar test"
+        }
+        res=self.client.patch(url,data)
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
+        
+    def test_update_role_anou_user(self):
+        self.client.credentials()
+        url=reverse('v1:post_urls-edit_and_get_role',args=[self.project.id,self.role.id])
+        data={
+            "title":"aman kumar test"
+        }
+        res=self.client.patch(url,data)
+        self.assertEqual(res.status_code,status.HTTP_401_UNAUTHORIZED)
+        
+    def test_update_other_user(self):
+        other_user=User.objects.create_user(
+            email="other@gmail.com",
+            password="Amankumar@144"
+        )
+        token=RefreshToken.for_user(other_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {str(token.access_token)}")
+        url=reverse('v1:post_urls-edit_and_get_role',args=[self.project.id,self.role.id])
+        data={
+            "title":"update title"
+        }
+        res=self.client.patch(url,data)
+        self.assertEqual(res.status_code,status.HTTP_403_FORBIDDEN)
+          
+        
+      
+        
+        
+        
         
 
                                             
